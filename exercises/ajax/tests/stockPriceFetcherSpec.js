10a05369501cc8100cc9fbfe122661a6a281f0b0
Alex York <alexyork1985@gmail.com>
2015-02-17 23:05:34 +0100

    Expanded the unit tests with jasmine.Ajax()

--- exercises/ajax/tests/stockPriceFetcherSpec.js
+++ exercises/ajax/tests/stockPriceFetcherSpec.js
@@ -67,25 +67,53 @@ describe("stockPriceFetcher", function () {
         expect(errorCallback).not.toHaveBeenCalled();
     });
 
-    xit("XHR tests", function() {
+});
+
+describe("stockPriceFetcher w/ jasmine.Ajax()", function() {
+
+    beforeEach(function() {
         jasmine.Ajax.install();
-
-        var successCallback = jasmine.createSpy();
-        stockFetcher.fetch("BOUVET", successCallback, emptyCallback);
-
-        var xhr = jasmine.Ajax.requests.mostRecent();
-        expect(xhr.url).toMatch(/BOUVET/);
-        expect(successCallback).not.toHaveBeenCalled();
-
-        jasmine.Ajax.requests.mostRecent().respondWith({
-            "status": 200,
-            "contentType": 'application/json',
-            "responseText": JSON.stringify({ fake: "json" })
-        });
-        
-        expect(successCallback).toHaveBeenCalled();
-
+    });
+    
+    afterEach(function() {
         jasmine.Ajax.uninstall();
     });
+    
+    it("should fetch data from the correct URL", function() {
+        var successCallback = jasmine.createSpy();
+        
+        var stockFetcher = jsWorkshop.stockPriceFetcher();
+        stockFetcher.fetch("BOUVET", successCallback);
 
+        var xhr = jasmine.Ajax.requests.mostRecent();
+        
+        expect(xhr.url).toMatch(/BOUVET/);
+    });
+    
+    it("should call the success callback when successful", function() {
+        var successCallback = jasmine.createSpy();
+        
+        var stockFetcher = jsWorkshop.stockPriceFetcher();
+        stockFetcher.fetch("BOUVET", successCallback);
+
+        server.doesNotRespondYet();
+        expect(successCallback).not.toHaveBeenCalled();
+
+        server.successfullyRespondsWith({ some: "json", data: "from the server" });
+        expect(successCallback).toHaveBeenCalled();
+    });
+    
+    // DSL-type object which makes the unit tests more pleasant
+    var server = {
+        doesNotRespondYet: function() {
+        },
+        successfullyRespondsWith: function(obj) {
+            jasmine.Ajax.requests.mostRecent().respondWith({
+                "status": 200,
+                "contentType": 'application/json',
+                "responseText": JSON.stringify(obj)
+            });
+        }
+    };
+    
 });
