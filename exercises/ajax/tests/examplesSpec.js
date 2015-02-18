b598976f7aedb8b1c5d882293a22c9526ebd0f7e
Alex York <alexyork1985@gmail.com>
2015-02-18 15:42:46 +0100

    Removed some tests

--- exercises/ajax/tests/examplesSpec.js
+++ exercises/ajax/tests/examplesSpec.js
@@ -11,93 +11,11 @@ function runMyAjaxCode(callback) {
     });
 }
 
-describe("overwriting $.ajax", function() {
+//
+// Unit tests
+describe("runMyAjaxCode", function() {
 
-    var $ajaxOriginalFn;
-
-    beforeEach(function() {
-        $ajaxOriginalFn = $.ajax;
+    it("should ???", function() {
     });
-
-    afterEach(function() {
-        $.ajax = $ajaxOriginalFn;
-    });
-
-    it("should call the callback", function() {
-        $.ajax = function(options) {
-            options.success();
-        };
-
-        var callbackWasCalled = false;
-        var ajaxCallback = function() {
-            callbackWasCalled = true;
-        };
-
-        runMyAjaxCode(ajaxCallback);
-        expect(callbackWasCalled).toBe(true);
-    });
-
-    it("should pass the data to the callback", function() {
-        $.ajax = function(options) {
-            options.success({ what: "the f**k?" });
-        };
-
-        var ajaxCallback = function(data) {
-            expect(data.what).toEqual("the f**k?");
-        };
-
-        runMyAjaxCode(ajaxCallback);
-    });
-
-});
-
-describe("using Jasmine spies", function() {
-
-    it("should call $.ajax", function() {
-        spyOn($, "ajax");
-
-        runMyAjaxCode(function() {});
-
-        expect($.ajax).toHaveBeenCalled();
-    });
-
-    it("should call my callback", function() {
-        spyOn($, "ajax").and.callFake(function(options) {
-            options.success();
-        });
-
-        var myCallbackSpy = jasmine.createSpy();
-        runMyAjaxCode(myCallbackSpy);
-
-        expect(myCallbackSpy).toHaveBeenCalled();
-    });
-
-});
-
-describe("faking AJAX responses with a fake server", function() {
-
-    beforeEach(function() {
-        jasmine.Ajax.install();
-    });
-
-    afterEach(function() {
-        jasmine.Ajax.uninstall();
-    });
-
-    it("should call my callback", function() {
-        var myCallbackSpy = jasmine.createSpy();
-        runMyAjaxCode(myCallbackSpy);
-
-        var xhr = jasmine.Ajax.requests.mostRecent();
-        expect(xhr.url).toMatch(/foo/);
-        expect(myCallbackSpy).not.toHaveBeenCalled();
-
-        jasmine.Ajax.requests.mostRecent().respondWith({
-            "status": 200,
-            "contentType": 'application/json',
-            "responseText": JSON.stringify({ fake: "json" })
-        });
-
-        expect(myCallbackSpy).toHaveBeenCalled();
-    });
+    
 });
--- exercises/time/examplesSpec.js
+++ exercises/time/examplesSpec.js
@@ -6,44 +6,11 @@ function doMyStuffLater(delay, callback) {
     }, delay);
 }
 
+//
+// Unit tests
 describe("doMyStuffLater", function() {
 
-    var callback;
-    
-    beforeEach(function() {
-        callback = jasmine.createSpy("timer callback spy");
-        jasmine.clock().install();
-    });
-
-    afterEach(function() {
-        jasmine.clock().uninstall();
-    });
-
-    it("should not call the callback immediately", function() {
-        doMyStuffLater(1000, callback);
-        expect(callback).not.toHaveBeenCalled();
+    it("should ???", function() {
     });
     
-    it("should call the callback after the delay", function() {
-        doMyStuffLater(1000, callback);
-        
-        jasmine.clock().tick(999);
-        expect(callback).not.toHaveBeenCalled();
-        
-        jasmine.clock().tick(2);
-        expect(callback).toHaveBeenCalled();
-    });
-    
-    it("should call the callback after the delay with the date", function() {
-        var baseDate = new Date(1985, 3, 17);
-        jasmine.clock().mockDate(baseDate);
-        
-        doMyStuffLater(1000, callback);
-        
-        jasmine.clock().tick(1001);
-        var actualDate = callback.calls.mostRecent().args[0];
-        
-        expect(actualDate.getTime()).toBe(baseDate.getTime() + 1001);
-    });
-
 });
